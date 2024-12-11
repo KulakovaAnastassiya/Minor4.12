12900c2c0a8c0b12bcce152f4acbecba0d80bd40
KulakovaAnastassiya <nastya.kulakova.2006@gmail.com>
2024-12-11 14:49:01 +0500

    Add files via upload

--- 1_api.js
+++ 1_api.js
@@ -1,22 +1,37 @@
-async function getResponce() {
-    let responce = await fetch("shop.json")
-    let content = await responce.text()
-    console.log(content)
-    content = JSON.parse(content)
-    console.log(content)
 
-    let key
-    for (key in content) {
-        console.log(content[key].id, content[key].title)
-        console.log(content[key])
-    }
 function sort() {
     let title = document.getElementById("title")
     if (title.checked) {
         document.getElementById('node_for_insert').innerHTML = '';
         getResponce1()
     }
+    let title1 = document.getElementById("title1")
+    if (title1.checked) {
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce2()
+    }
 }
+async function getResponce2() {
+    let responce = await fetch("shop.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 11)
+    console.log(content)
+} ;
+let node_for_insert = document.getElementById("node_for_insert")
+for (key in content_title) {
+            node_for_insert.innerHTML += `
+            <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
+            <img style="width: 180px" class="align-self-center" src=${content[key].img}>
+            <h5 class="card-title">${content[key].title}</h5>
+            <p class="card-text">${content[key].description}</p>
+            <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
+            <p class="card-text" >Хочу узнать больше<input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
+            </li>
+                    `
+        }
 async function getResponce1() {
     let responce = await fetch("shop.json")
 
@@ -55,8 +70,3 @@ async function getResponce1() {
 }
 
 sort()
-
-
-
-}
-getResponce()
